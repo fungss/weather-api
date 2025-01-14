b30b27aa669e41426e8c291d3225b3020e6cd849
Ronald Fung <rfung.dev@icloud.com>
2025-01-14 21:45:14 +0000

    Add ConfigModule and initiate WeatherModule (#1)
    * Add ConfigModule and initiate WeatherModule
    
    * Update src/weather/weather.module.ts
    
    Co-authored-by: Matthew Wong <matthew.chohin@gmail.com>
    
    * Update visual crossing api key var name
    
    ---------
    
    Co-authored-by: Matthew Wong <matthew.chohin@gmail.com>

---
+++ src/weather/weather.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class WeatherService {
+    private readonly visualCrossingApiKey: string;
+    
+    constructor(private configService: ConfigService){
+        this.visualCrossingApiKey = this.configService.get<string>('VISUAL_CROSSING_API_KEY');
+    }
+    
+    async findByLocation(location: string) {
+        return `you entered ${location}`;
+    }
+}
