a425b87556fd309fb022b065e14089c1756fbd20
Ronald Fung <rfung.dev@icloud.com>
2025-01-14 21:45:14 +0000

    Add ConfigModule and initiate WeatherModule (#1)
    * Add ConfigModule and initiate WeatherModule
    
    * Update src/weather/weather.module.ts
    
    Co-authored-by: Matthew Wong <matthew.chohin@gmail.com>
    
    * Update visual crossing api key var name
    
    ---------
    
    Co-authored-by: Matthew Wong <matthew.chohin@gmail.com>

---
+++ src/weather/weather.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { WeatherController } from './weather.controller';
+import { WeatherService } from './weather.service';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: process.env.NODE_ENV === 'production'
+    })
+  ],
+  controllers: [WeatherController],
+  providers: [WeatherService]
+})
+export class WeatherModule {}
